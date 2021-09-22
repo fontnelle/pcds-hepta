a3fea4ee8cd7a5b03c0694e3a2b1125e9bb96535
João Henrique <fontnelle@hotmail.com>
2021-09-21 22:03:01 -0300

    Ajustando header e classes

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,15 +12,15 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     let tl = new TimelineMax();
 
-    tl.fromTo('.bg-loader', 1 ,
+    tl.fromTo('', 1 ,
     {width: '100%'},
-    {width: '0%', display: 'none', opacity:0, delay: 6, ease: Expo.easeInOut})
+    {width: '0%', display: 'none', opacity:0, delay: 0.1, ease: Expo.easeInOut})
 
     .fromTo('.logo', 1 ,
     {y:-50, opacity:0},
     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.5')
 
-   
+
     .fromTo('.ani1', .5,
     {y:-50, opacity:0},
     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
