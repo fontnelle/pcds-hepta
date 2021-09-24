057169a1ea80307b3352a94c8d068777a724d60b
João Henrique <fontnelle@hotmail.com>
2021-09-23 23:54:42 -0300

    Alterando header, img e texto

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,52 +8,52 @@ window.addEventListener('scroll', scrollHeader)
 
 
 
-document.addEventListener('DOMContentLoaded', () =>{
+// document.addEventListener('DOMContentLoaded', () =>{
 
-    let tl = new TimelineMax();
+//     let tl = new TimelineMax();
 
-    tl.fromTo('', 1 ,
-    {width: '100%'},
-    {width: '0%', display: 'none', opacity:0, delay: 0.1, ease: Expo.easeInOut})
+//     tl.fromTo('', 1 ,
+//     {width: '100%'},
+//     {width: '0%', display: 'none', opacity:0, delay: 0.1, ease: Expo.easeInOut})
 
-    .fromTo('.logo', 1 ,
-    {y:-50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.5')
+//     .fromTo('.logo', 1 ,
+//     {y:-50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.5')
 
 
-    .fromTo('.ani1', .5,
-    {y:-50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
+//     .fromTo('.ani1', .5,
+//     {y:-50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
 
-    .fromTo('.ani2', .5,
-    {y:-50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
+//     .fromTo('.ani2', .5,
+//     {y:-50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
 
-    .fromTo('.ani3', .5,
-    {y:-50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
+//     .fromTo('.ani3', .5,
+//     {y:-50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
 
-    .fromTo('.ani4', .5,
-    {y:50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
+//     .fromTo('.ani4', .5,
+//     {y:50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
 
-    .fromTo('.ani5', 1,
-    {y:-50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.1')
+//     .fromTo('.ani5', 1,
+//     {y:-50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.1')
 
-    .fromTo('.ani6', 1,
-    {y:50, opacity:0},
-    {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
+//     .fromTo('.ani6', 1,
+//     {y:50, opacity:0},
+//     {y:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
 
-    .fromTo('.ani7', 1,
-    {x:50, opacity:0},
-    {x:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
+//     .fromTo('.ani7', 1,
+//     {x:50, opacity:0},
+//     {x:0, opacity:1, ease: Expo.easeInOut}, '-=0.2')
 
-    .fromTo('.ani8', 1,
-    {x:-50, opacity:0},
-    {x:0, opacity:1, ease: Expo.easeInOut}, '-=0.8')
+//     .fromTo('.ani8', 1,
+//     {x:-50, opacity:0},
+//     {x:0, opacity:1, ease: Expo.easeInOut}, '-=0.8')
     
-})
+// })
 
 
 
